772feb8513abeed37e8b8da96ca3cca8385bd231
Alex Coelho <alexcastelo437@mail.com>
2025-10-29 09:16:24 -0300

    feat: definido controller de aluno

---
+++ src/aluno/aluno.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode } from '@nestjs/common';
+import { AlunoService } from './aluno.service';
+import { CreateAlunoDto } from './dto/create-aluno.dto';
+import { UpdateAlunoDto } from './dto/update-aluno.dto';
+
+@Controller('alunos')
+export class AlunoController {
+  constructor(private readonly alunoService: AlunoService) {}
+
+  @Post()
+  @HttpCode(201)
+  async criarAluno(@Body() createAlunoDto: CreateAlunoDto) {
+    await this.alunoService.create(createAlunoDto);
+  }
+
+  @Get()
+  @HttpCode(200)
+  listarAlunos() {
+    return this.alunoService.findAll();
+  }
+
+  @Get(':id')
+  @HttpCode(200)
+  listarUmAluno(@Param('id') id: string) {
+    return this.alunoService.findOne(id);
+  }
+
+  @Patch(':id')
+  @HttpCode(204)
+  atualizarAluno(@Param('id') id: string, @Body() updateAlunoDto: UpdateAlunoDto) {
+    return this.alunoService.update(id, updateAlunoDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  deletarAluno(@Param('id') id: string) {
+    return this.alunoService.remove(id);
+  }
+}
