279d5ddc05fdbe8ec17958fdf3a24429787abbf0
Alex Coelho <alexcastelo437@mail.com>
2025-11-01 22:56:56 -0300

    feat: adicionando import de instrutor em aula pratica

--- src/aula-pratica/aula-pratica.module.ts
+++ src/aula-pratica/aula-pratica.module.ts
@@ -3,9 +3,10 @@ import { AulaPraticaService } from './aula-pratica.service';
 import { AulaPraticaController } from './aula-pratica.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AulaPratica } from 'src/entity/AulaPratica.entity';
+import { Instrutor } from 'src/entity/Instrutor.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AulaPratica])],
+  imports: [TypeOrmModule.forFeature([AulaPratica, Instrutor])],
   controllers: [AulaPraticaController],
   providers: [AulaPraticaService],
 })
